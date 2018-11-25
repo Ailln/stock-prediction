d5297223a90d92b5fab1859f53d753c604bdd9aa
htb <kinggreenhall@gmail.com>
2018-11-26 05:57:03 +0800

    update readme.

---
+++ models/sklearn_model.py
@@ -0,0 +1,22 @@
+from sklearn import svm
+from sklearn import ensemble
+from sklearn import linear_model
+
+
+class Model(object):
+    def __init__(self):
+        self.model_dict = {
+            "SGDRegressor": linear_model.SGDRegressor(),
+            "HuberRegressor": linear_model.HuberRegressor(),
+            "LinearRegression": linear_model.LinearRegression(),
+            "SVR": svm.SVR(),
+            "LinearSVR": svm.LinearSVR(),
+            "BaggingRegressor": ensemble.BaggingRegressor(),
+            "AdaBoostRegressor": ensemble.AdaBoostRegressor(),
+            "ExtraTreesRegressor": ensemble.ExtraTreesRegressor(),
+            "RandomForestRegressor": ensemble.RandomForestRegressor(),
+            "GradientBoostingRegressor": ensemble.GradientBoostingRegressor()
+        }
+
+    def sklearn_model(self, model_name):
+        return self.model_dict[model_name]
--- run/sklearn.py
+++ run/sklearn.py
@@ -3,7 +3,7 @@ from sklearn.metrics import mean_squared_error
 
 from utils import data_utils
 from utils import config_utils
-from models.sklearn import sklearn_model
+from models import sklearn_model
 
 
 def train(config_path):
@@ -14,19 +14,21 @@ def train(config_path):
     du = data_utils.DataUtils(config)
     train_input, train_target, validate_input, validate_target = du.get_train_and_validate_data()
 
-    model = sklearn_model(model_name)
+    model = sklearn_model.Model()
     print(f">> run {model_name} model...")
-    model.fit(train_input, train_target)
+    skm = model.sklearn_model(model_name)
+    skm.fit(train_input, train_target)
 
     print(">> save model...")
     joblib.dump(model, config["models"]["model_path"] + f"/{model_name}.m")
 
-    print(">> predict...")
-    validate_preds = model.predict(validate_input)
+    print(">> predict...\n")
+    validate_preds = skm.predict(validate_input)
 
+    print("## index: predict target")
     for index in range(len(validate_target)):
         if (index + 1) <= 10:
-            print(index, validate_preds[index], validate_target[index])
+            print(f">> {index}: {validate_preds[index]} {validate_target[index]}")
         else:
             break
 
