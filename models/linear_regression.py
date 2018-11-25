cbef809e95f9630830d057fbce4172b75d1e93ae
htb <kinggreenhall@gmail.com>
2018-11-25 15:05:23 +0800

    split train and validate datas.

---
+++ models/linear_regression.py
@@ -0,0 +1,7 @@
+from sklearn import linear_model
+
+
+def linear_regression():
+    clf = linear_model.LinearRegression()
+
+    clf.fit()
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,14 +4,48 @@ import pandas as pd
 
 from utils import config_utils
 
-
-config_path = "./config.yaml"
-config = config_utils.read_config(config_path)
+is_debug = True
 
 
-def merge_date_data():
-    train_path = Path(config["datas"]["train_path"])
-    for date_path in train_path.glob("*"):
+class DataUtils(object):
+    def __init__(self, config_path):
+        self.config = config_utils.read_config(config_path)
+
+        self.origin_train_path = Path(self.config["datas"]["origin_train_path"])
+        self.origin_test_path = Path(self.config["datas"]["origin_test_path"])
+        self.generate_train_path = Path(self.config["datas"]["generate_train_path"])
+        self.generate_validate_path = Path(self.config["datas"]["generate_validate_path"])
+        self.generate_test_path = Path(self.config["datas"]["generate_test_path"])
+
+        self.is_regenerate_train_and_validate_data = self.config["datas"]["is_regenerate_train_and_validate_data"]
+        self.split_validate_size = self.config["datas"]["split_validate_size"]
+
+    def generate_train_and_validate_data(self):
+        if is_debug:
+            self.is_regenerate_train_and_validate_data = 1
+
+        if self.is_regenerate_train_and_validate_data:
+            sorted_all_data_path = sorted(self.origin_train_path.glob("*"))
+            sorted_all_data_len = len(sorted_all_data_path)
+            print(f">> all data length: {sorted_all_data_len}")
+
+            df_train_data = pd.DataFrame()
+            df_validate_data = pd.DataFrame()
+            for index, date_path in enumerate(sorted_all_data_path):
+                if index <= sorted_all_data_len * (1 - self.split_validate_size):
+                    df_train_data = pd.concat([df_train_data, self.__merge_date_data(date_path)], ignore_index=True)
+                else:
+                    df_validate_data = pd.concat([df_validate_data, self.__merge_date_data(date_path)],
+                                                 ignore_index=True)
+
+                if not index % 10:
+                    print(f">> {index}")
+
+            df_train_data.to_csv(self.generate_train_path)
+            df_validate_data.to_csv(self.generate_validate_path)
+            print(">> generate train and validate data success !")
+
+    def __merge_date_data(self, date_path):
         df_non_ts = pd.read_csv(date_path / "non_ts.csv", index_col="id")
         df_y = pd.read_csv(date_path / "y.csv")
         merge_df = pd.merge(df_non_ts, df_y, on="id")
@@ -20,33 +54,34 @@ def merge_date_data():
             ts_name = ts_path.name.split(".csv")[0]
             df_ts = pd.read_csv(ts_path, index_col="id")
 
-            df_ts_std = get_std(df_ts, ts_name)
+            df_ts_std = self.__get_std(df_ts, ts_name)
             merge_df = pd.merge(merge_df, df_ts_std, on="id")
 
-            df_ts_mean_0_5 = get_mean_0_5(df_ts, ts_name)
+            df_ts_mean_0_5 = self.__get_mean_0_5(df_ts, ts_name)
             merge_df = pd.merge(merge_df, df_ts_mean_0_5, on="id")
 
-            df_ts_mean_0_20 = get_mean_0_20(df_ts, ts_name)
+            df_ts_mean_0_20 = self.__get_mean_0_20(df_ts, ts_name)
             merge_df = pd.merge(merge_df, df_ts_mean_0_20, on="id")
 
-        print(merge_df.head())
-        break
+        return merge_df
 
+    @staticmethod
+    def __get_std(df_input, index_name):
+        df_output = df_input.T[1:].std().to_frame(name=index_name+"_std")
+        return df_output
 
-def get_std(df_input, index_name):
-    df_output = df_input.T[1:].std().to_frame(name=index_name+"_std")
-    return df_output
+    @staticmethod
+    def __get_mean_0_5(df_input, index_name):
+        df_output = df_input.T[1:7].mean().to_frame(name=index_name+"_mean_0_5")
+        return df_output
 
-
-def get_mean_0_5(df_input, index_name):
-    df_output = df_input.T[1:7].mean().to_frame(name=index_name+"_mean_0_5")
-    return df_output
-
-
-def get_mean_0_20(df_input, index_name):
-    df_output = df_input.T[1:22].mean().to_frame(name=index_name+"_mean_0_20")
-    return df_output
+    @staticmethod
+    def __get_mean_0_20(df_input, index_name):
+        df_output = df_input.T[1:22].mean().to_frame(name=index_name+"_mean_0_20")
+        return df_output
 
 
 if __name__ == '__main__':
-    merge_date_data()
+    my_config_path = "./config.yaml"
+    du = DataUtils(my_config_path)
+    du.generate_train_and_validate_data()
